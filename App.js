1ea04862dd57bdbae832c1d58f3aef9305e1c411
nineonine <mail4chemik@gmail.com>
2017-06-22 15:02:41 -0700

    change App.js + README

--- App.js
+++ App.js
@@ -10,11 +10,7 @@ export default class App extends React.Component {
   }
 }
 
-{/* const styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-}); */}
+});
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 AirfareSpot React Native app
+
+TODO:
+1) list of deals page. Deal name, photo, from, to, date, airlines,
+2) private account page / Settings
+3) ad support
