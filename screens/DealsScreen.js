41394716870fc2b9a6191f1eb2aa9784c8580e05
nineonine <mail4chemik@gmail.com>
2017-06-21 22:04:09 -0700

    implement basic deals list

--- screens/DealsScreen.js
+++ screens/DealsScreen.js
@@ -1,37 +1,99 @@
 import React, { Component } from 'react';
-import { FlatList, StyleSheet, Text, View, Image } from 'react-native';
+import { ActivityIndicator, FlatList, StyleSheet, Text, View, Image } from 'react-native';
 
-let data = [
-    {title: "Los Angeles to San Francisco", pubDate: "2017-06-17 17:30:14", key:1}
-  , {title: "Charlotte to Los Angeles", pubDate: "2017-06-17 16:41:39", key:2}
-  , {title: "Dallas to Washington", pubDate: "2017-06-17 16:21:32", key:3}
-  , {title: "Philadelphia to New Orleans", pubDate: "2017-06-17 15:59:29", key:4}
-  , {title: "Fort Lauderdale to Aguadilla", pubDate: "2017-06-17 15:40:55", key:5}
-]
+let Api = require('../api/DealsApi');
+
+let ddata = [
+  {title: "Los Angeles to San Francisco", date: "2017-06-17 17:30:14", key:1}
+, {title: "Charlotte to Los Angeles", date: "2017-06-17 16:41:39", key:2}
+, {title: "Dallas to Washington", date: "2017-06-17 16:21:32", key:3}
+, {title: "Philadelphia to New Orleans", date: "2017-06-17 15:59:29", key:4}
+, {title: "Fort Lauderdale to Aguadilla", date: "2017-06-17 15:40:55", key:5}
+];
 
 export default class DealsScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        deals: []
+      , dealsLoaded: false
+    }
+  }
+
+  _keyExtractor = (item, ix) => item.id;
+
+  onAfterDealsLoad = (deals) => {
+    deals.map( v => {console.log(v.id); return v} );
+    this.setState( (prevSt) => {
+      return{deals: deals, dealsLoaded: true}
+    })
+  }
+
+  componentDidMount() {
+
+      Api.fetchRecentDeals()
+        .then( (deals) => this.onAfterDealsLoad(deals) )
+        .catch( (e) => {
+          alert('Something went wrong! :(' + e.message);
+          console.log(e)
+        })
+
+  }
 
 
   render() {
+
+    const getAllTags = (a) => {
+      return a.categories.map(c => c.title)
+    }
+
+    const renderTags = (ts) => {
+      return (
+        ts.map( t => {
+          return (
+            <Text style={styles.dealTag}>{"#" + t}</Text>
+          )
+        })
+      )
+    }
+
+    if (!this.state.dealsLoaded) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
     return (
       <View style={styles.container}>
         <FlatList
-          data={data}
+          data= {this.state.deals}
+          keyExtractor= {this._keyExtractor}
           renderItem= { ({item}) =>
+
             <View style={styles.dealContainer}>
+
                 <View style={styles.dealContHeader}>
-                    <Text style={styles.pubTime}>{item.pubDate}</Text>
+                    <Text style={styles.pubTime}>{item.date}</Text>
                 </View>
+
                 <View style={styles.dealContBody}>
-                    <Text style={styles.item}>
-                      {item.title}
-                    </Text>
+
+                    <View style={styles.dealTextBlock}>
+                      <Text style={styles.dealTitle}>
+                        {item.title}
+                      </Text>
+                      <View style={styles.dealTags}>
+                          {renderTags(getAllTags(item))}
+                      </View>
+                    </View>
+
                     <Image
-                      source={{uri:'https://i.ytimg.com/vi/QX4j_zHAlw8/maxresdefault.jpg'}}
-                    //   source={require('../assets/images/deal-placeholder-icon.jpg')}
-                      style={styles.dealImage}
-                    />
+                      source={{uri: item.thumbnail}}
+                      style={styles.dealImage}/>
                 </View>
+
             </View>
           }
         />
@@ -45,11 +107,25 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 22
   },
-  item: {
-    padding: 7,
-    fontSize: 18,
-    height: 44,
+  dealTextBlock: {
     flex: 3,
+    paddingTop: 5,
+    flexDirection: 'column',
+  },
+  dealTags: {
+    paddingTop: 8,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  dealTag: {
+    backgroundColor: '#eee',
+    padding: 3,
+    margin: 3,
+    fontSize: 12,
+  },
+  dealTitle: {
+    fontWeight: '500',
+    fontSize: 16,
   },
   dealContainer: {
     borderWidth: 1,
@@ -60,7 +136,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'flex-end',
     alignItems: 'center',
-    backgroundColor: '#ffeeed',
+    backgroundColor: '#efefef',
     height: 30,
   },
   dealContBody: {
@@ -71,11 +147,11 @@ const styles = StyleSheet.create({
   dealImage: {
     width: 100,
     height: 100,
-    // flex: 1,
     borderRadius: 50,
+    marginLeft: 15,
   },
   pubTime: {
     paddingRight: 10,
-
+    color: '#6b6b6b',
   },
 })
