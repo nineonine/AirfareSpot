1e6a33af52790dc3dfb71902a7ff4d9ad0f7dbc3
nineonine <mail4chemik@gmail.com>
2017-06-24 16:30:59 -0700

    eject project

---
+++ android/app/src/main/java/com/android/909/generated/ExponentBuildConstants.java
@@ -0,0 +1,8 @@
+package com.android.909.generated;
+
+// This file is auto-generated. Please don't rename!
+public class ExponentBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "";
+
+}
